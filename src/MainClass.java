2d7a9a65fc978427041e1c250083ecd406f22d8f
Vladimir Shkolnyi <DiastroniX@gmail.com>
2020-01-22 23:04:39 +0300

    Test 2. Delete "this."!

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,15 @@
 public class MainClass
 {
-    public int getLocalNumber()
+    static int class_number = 20;
+
+
+    public static int getLocalNumber()
     {
         return 14;
+    }
+
+    public static int getClassNumber()
+    {
+        return class_number;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,13 +6,26 @@ public class MainClassTest extends MainClass
     @Test
     public void testGetLocalNumber()
     {
-        if (this.getLocalNumber() == 14)
+        if (getLocalNumber() == 14)
         {
             System.out.println("Passed, Local Number = 14");
         }
         else
         {
             Assert.fail("Fail, Local Number != 14");
+        }
+    }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        if (getClassNumber() > 45)
+        {
+            System.out.println("Passed, Class Number > 45");
+        }
+        else
+        {
+            Assert.fail("Fail, Class Number <= 45");
         }
     }
 }
